70c060941647a88c7d5261889767864050196066
Seiji <122525189+justseiji@users.noreply.github.com>
2025-02-11 12:41:12 +0800

    salon.sh

--- salon.sh
+++ salon.sh
@@ -1,55 +1,55 @@
 #!/bin/bash
 
-PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
-echo -e "\n~~~~~ MY SALON ~~~~~\n"
-echo -e "Welcome to My Salon, how can I help you?\n"
+echo "~~~~~ MY SALON ~~~~~"
+echo "Welcome to My Salon, how can I help you?"
 
-GET_SERVICES_ID() {
-  if [[ $1 ]]
-  then
-    echo -e "\n$1"
-  fi
+
+psql --username=freecodecamp --dbname=salon -c "SELECT service_id, name FROM services;" | tail -n +3 | head -n -2 | while read SERVICE_ID SERVICE_NAME
+do
+    echo "$SERVICE_ID) $SERVICE_NAME"
+done
+
+
+while true; do
+    echo "What service would you like today? (Please enter the number)"
+    read SERVICE_ID_SELECTED
+    SERVICE_NAME=$(psql --username=freecodecamp --dbname=salon -t -c "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED;")
+
+    if [[ -z "$SERVICE_NAME" ]]; then
+        echo "I could not find that service. What would you like today?"
+        # Re-display the list of services
+        psql --username=freecodecamp --dbname=salon -c "SELECT service_id, name FROM services;" | tail -n +3 | head -n -2 | while read SERVICE_ID SERVICE_NAME
+        do
+            echo "$SERVICE_ID) $SERVICE_NAME"
+        done
+    else
+        break
+    fi
+done
+
+
+echo "What's your phone number?"
+read CUSTOMER_PHONE
+
+
+CUSTOMER_EXISTS=$(psql --username=freecodecamp --dbname=salon -t -c "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE';")
+
+if [[ -z "$CUSTOMER_EXISTS" ]]; then
   
-  LIST_SERVICES=$($PSQL "SELECT * FROM services")
-  echo "$LIST_SERVICES" | while read SERVICE_ID BAR SERVICE
-  do
-    ID=$(echo $SERVICE_ID | sed 's/ //g')
-    NAME=$(echo $SERVICE | sed 's/ //g')
-    echo "$ID) $SERVICE"
-  done
-  read SERVICE_ID_SELECTED
-  case $SERVICE_ID_SELECTED in
-    [1-5]) NEXT ;;
-        *) GET_SERVICES_ID "I could not find that service. What would you like today?" ;;
-  esac
-}
-
-NEXT() {
-  echo -e "\nWhat's your phone number?"
-  read CUSTOMER_PHONE
-  # CUSTOMER_PHONE_FORMATED=$(echo $CUSTOMER_PHONE | sed 's/[^0-9]*//g')
-  NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
-  CUSTOMER_NAME=$(echo $NAME | sed 's/ //g')
-  if [[ -z $NAME ]]
-  then
-    echo -e "\nI don't have a record for that phone number, what's your name?"
+    echo "I don't have a record for that phone number, what's your name?"
     read CUSTOMER_NAME
-    NAME=$(echo $NAME | sed 's/ //g')
-    SAVED_TO_TABLE_CUSTOMERS=$($PSQL "INSERT INTO customers(name,phone) VALUES('$NAME','$CUSTOMER_PHONE')")
-  fi
-  
-  GET_SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-  SERVICE_NAME=$(echo $GET_SERVICE_NAME| sed 's/ //g')
-  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
-  
-  echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
-  read SERVICE_TIME
-  SAVED_TO_TABLE_APPOINTMENTS=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
-  if [[ $SAVED_TO_TABLE_APPOINTMENTS == "INSERT 0 1" ]]
-  then
-    echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
-  fi
+    psql --username=freecodecamp --dbname=salon -c "INSERT INTO customers (name, phone) VALUES ('$CUSTOMER_NAME', '$CUSTOMER_PHONE');"
+    CUSTOMER_ID=$(psql --username=freecodecamp --dbname=salon -t -c "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE';")
+else
+ 
+    CUSTOMER_ID=$(psql --username=freecodecamp --dbname=salon -t -c "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE';")
+    echo "Welcome back, $CUSTOMER_NAME!"
+fi
 
-}
+echo "What time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
+read SERVICE_TIME
 
-GET_SERVICES_ID
+psql --username=freecodecamp --dbname=salon -c "INSERT INTO appointments (customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME');"
+
+
+echo "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
